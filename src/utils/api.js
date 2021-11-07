6ee1065eb2542663d2928ef19de7fbb9bf43fb08
怡瑄 <a6687225@gmail.com>
2021-11-07 23:09:59 +0800

    api test
    api test

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import * as common  from "@/utils/common";
 
 //觀光相關
-const tourRequest = axios.create({
-    baseURL: "https://ptx.transportdata.tw/MOTC/v2/Tourism/",
-    dataType: 'json',
+const api = axios.create({
+    baseURL: "https://ptx.transportdata.tw/MOTC/v2/",
+    dataType: "json",
     headers: common.getAuthorizationHeader(),
 });
 
-export const apiTour = parm => tourRequest.get(`/ScenicSpot?${parm}`);
+export const apiTourScene = parm => api.get(`Tourism/ScenicSpot?${parm}`);
 // https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$top=30&$format=JSON
 //https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=30&$format=JSON
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,27 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-
+import { apiTourScene } from "@/utils/api.js";
 export default {
   name: "Home",
   components: {
     HelloWorld,
   },
+  methods: {
+    async getTourData() {
+      try {
+        const result = await apiTourScene("$top=30");
+        if(result.status === 200) {
+          console.log("success");
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  },
+  mounted() {
+    this.getTourData();
+  }
 };
 </script>
